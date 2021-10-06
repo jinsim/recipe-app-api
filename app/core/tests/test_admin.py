b1bd30d78691df4136ad13dfe10b6c88699595a2
jinsim <simsime@korea.ac.kr>
2021-10-06 18:49:06 +0900

    Updated admin to support custom user model

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,44 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+class AdminSiteTests(TestCase):
+    # 가장 먼저 setup 함수를 만든다. 
+    def setUp(self):
+        # 테스트하는데 사용할 유저를 만들 거임. 로그인되어있는지 확인, 인증되지 않았거나 
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@londonappdev.com',
+            password = 'password123'
+        )
+        # 사용자가 수동으로 로그인할 필요 없이 이거 누르면 된다. 
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@londonappdev.com',
+            password='password123',
+            name='Test user full name'
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        # 우리의 응답이 특정 항목을 포함하는지를 확인하는 django 사용자 정의 assertion이다. 
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+    
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        # /admin/core/user/1
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+        
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
