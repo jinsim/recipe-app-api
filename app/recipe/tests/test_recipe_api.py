43cfaf20dd95dbf01659322ee541414925289e5f
jinsim <simsime@korea.ac.kr>
2021-10-25 19:00:39 +0900

    Added list recipe feature

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -5,18 +5,34 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Recipe
+from core.models import Recipe, Tag, Ingredient
 
-from recipe.serializers import RecipeSerializer
-
+from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
 RECIPES_URL = reverse('recipe:recipe-list')
 
 
-# 테스트에서 반복적으로 객체가 필요할 때 도와주는 도우미 함수.
-# **params로 인해, 사용자가 추가로 전달한 모든 추가 매개변수가 params라는 사전으로 전달된다.
+def detail_url(recipe_id):
+    # api/recipes/id 에서 id가 항상 바뀌므로 list처럼 표준 url 설정이 힘듦.
+    """Return recipe detail URL"""
+    return reverse('recipe:recipe-detail', args=[recipe_id])
+
+
+def sample_tag(user, name='Main course'):
+    """Create and return a sample tag"""
+    # 샘플 tag와 ingredient 객체를 생성하는 코드
+    return Tag.objects.create(user=user, name=name)
+
+
+def sample_ingredient(user, name='Cinnamon'):
+    """Create and return a sample ingredient"""
+    return Ingredient.objects.create(user=user, name=name)
+
+
 def sample_recipe(user, **params):
     """Create and return a sample recipe"""
+    # 테스트에서 반복적으로 객체가 필요할 때 도와주는 도우미 함수.
+    # **params로 인해, 사용자가 추가로 전달한 모든 추가 매개변수가 params라는 사전으로 전달된다.
     defaults = {
         'title': 'Sample recipe',
         'time_minutes': 10,
@@ -79,4 +95,18 @@ class PrivateRecipeApiTests(TestCase):
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_view_recipe_detail(self):
+        """Test viewing a recipe detail"""
+        recipe = sample_recipe(user=self.user)
+        # n:n이라서 리스트에 추가하는 것임.
+        # 2번째 인자를 비워서 디폴트 name이 들어감.
+        recipe.tags.add(sample_tag(user=self.user))
+        recipe.ingredients.add(sample_ingredient(user=self.user))
+
+        url = detail_url(recipe.id)
+        res = self.client.get(url)
+
+        serializer = RecipeDetailSerializer(recipe)
         self.assertEqual(res.data, serializer.data)
